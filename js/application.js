e82af8cfe5e078548cff8a785f6b3c26e401e5e5
Odinei Rocha <odinei.ribeiro@live.com>
2017-01-25 02:28:09 -0200

    Added message to movie not found

--- js/application.js
+++ js/application.js
@@ -25,20 +25,24 @@ function movieSearch(movieName) {
 }
 
 function processSearchResult (result) {
-  search = result.Search;
+  if (result.Response.toLowerCase() == "true") {
+    search = result.Search;
 
-  var t = "";
-  for (var i = 0; i < search.length; i++){
-    var tr = "<tr>";
-    tr += "<td>" + search[i].Title + "</td>";
-    tr += "<td>" + search[i].Year + "</td>";
-    tr += "<td>" + search[i].Type + "</td>";
-    tr += "<td><a title=\"Movie page\" href=" + 'http://www.omdbapi.com/?i=' + search[0].imdbID + '&plot=full&r=json' + "><span class=\"glyphicon glyphicon-film\"></span></a></td>";
-    tr += "</tr>";
-    t += tr;
+    var t = "";
+    for (var i = 0; i < search.length; i++){
+      var tr = "<tr>";
+      tr += "<td>" + search[i].Title + "</td>";
+      tr += "<td>" + search[i].Year + "</td>";
+      tr += "<td>" + search[i].Type + "</td>";
+      tr += "<td><a title=\"Movie page\" href=" + 'http://www.omdbapi.com/?i=' + search[0].imdbID + '&plot=full&r=json' + "><span class=\"glyphicon glyphicon-film\"></span></a></td>";
+      tr += "</tr>";
+      t += tr;
+    }
+    document.getElementById("result").style.visibility = 'visible'
+    document.getElementById("table-result").innerHTML = t;
+  } else {
+    alert(result.Error);
   }
-  document.getElementById("result").style.visibility = 'visible'
-  document.getElementById("table-result").innerHTML = t;
 }
 
 String.prototype.gsub = function(search, replacement) {
